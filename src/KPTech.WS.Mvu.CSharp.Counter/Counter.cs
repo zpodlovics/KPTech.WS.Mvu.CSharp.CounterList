ee5d56e6c1231e42083abef9a06b4db2c928adbf
ZP <zpodlovics@users.noreply.github.com>
2019-06-18 22:57:11 +0200

    Fix header

--- src/KPTech.WS.Mvu.CSharp.Counter/Counter.cs
+++ src/KPTech.WS.Mvu.CSharp.Counter/Counter.cs
@@ -1,3 +1,10 @@
+// ---------------------------------------------------------------------------
+// Copyright (c) 2019, Zoltan Podlovics, KP-Tech Kft. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0. See LICENSE.md in the 
+// project root for license information.
+// ---------------------------------------------------------------------------
+
 using System;
 using WebSharper;
 using WebSharper.UI;
